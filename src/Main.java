940886fca1d3516c19a32b3c6f4c22ca75201e16
annamoskviceva <Annab.spb@mail.ru>
2023-08-06 20:35:40 +0300

    add 10

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int apples = 10;
+        System.out.println("Hello world!"+ apples);
     }
 }
